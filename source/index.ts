2487186e457acd66aa5a20d0198c2be3dfcb21c6
Upsided <matthewlloydw@gmail.com>
2022-07-31 03:26:43 +0000

    Added filesIn method

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,5 @@
+import { readdir } from 'fs/promises'
+
 /**
  * Returns a random integer between min (inclusive) and max (inclusive).
  * @param min The minimum value of the range.
@@ -7,4 +9,21 @@ export function randint(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export default { randint }
+/**
+ * Returns the list of filenames in a directory, optionally filtered by extension
+ * @param dir The directory to read.
+ * @param ext The extension(s) to filter by.
+ */
+export async function filesIn(dir: string, ext?: string | string[]) {
+    return (await readdir(dir)).filter(file => {
+        if (!ext) return true
+
+        if (typeof ext === 'string') {
+            return file.endsWith(ext)
+        } else {
+            return ext.some(e => file.endsWith(e))
+        }
+    })
+}
+
+export default { randint, filesIn }
